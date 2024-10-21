108478885a23348bf09cb3dd631226573f49103d
Ty Facey <justfacey@gmail.com>
2024-10-21 06:14:59 -0400

    Update xss_vuln.js

--- xss_vuln.js
+++ xss_vuln.js
@@ -6,7 +6,7 @@ router.get('/greeting', (req, res) => {
     res.send('<h1> Hello :'+ name +"</h1>")
 })
 
-router.get('/greet-template-demo', (req,res) => {
+router.get('/greet-template-', (req,res) => {
     name = req.query.name
     res.render('index', { user_name: name});
 })
