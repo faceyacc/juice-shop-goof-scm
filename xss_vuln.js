dabc2eacaf8ec2c6a1f72d3ff7d1bc9f38d43bb0
faceyacc <justfacey@gmail.com>
2024-10-21 07:49:16 -0400

    test

--- xss_vuln.js
+++ xss_vuln.js
@@ -6,7 +6,7 @@ router.get('/greeting', (req, res) => {
     res.send('<h1> Hello :'+ name +"</h1>")
 })
 
-router.get('/hello snyk', (req,res) => {
+router.get('/hello snyk--', (req,res) => {
     name = req.query.name
     res.render('index', { user_name: name});
 })
